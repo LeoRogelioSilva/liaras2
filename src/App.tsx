c13c6b4e92c7da8b9c74fb3009f60214190351b1
LeooRogelioSilva <leonardo-silva@vrsoft.com.br>
2024-11-29 23:51:56 -0300

    Contagem regressiva para o casamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,52 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
+interface TimeDifference {
+  value: number;
+  label: string;
+}
+
 const App: React.FC = () => {
   const [timeDifference, setTimeDifference] = useState({
-    years: 0,
-    months: 0,
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
+    years: { value: 0, label: "" },
+    months: { value: 0, label: "" },
+    days: { value: 0, label: "" },
+    hours: { value: 0, label: "" },
+    minutes: { value: 0, label: "" },
+    seconds: { value: 0, label: "" },
   });
 
   useEffect(() => {
-    const targetDate = new Date("2023-11-08T20:00:00.000Z");
+    const targetDate = new Date("2023-12-12T22:00:00.000Z");
 
     const calculateDifference = () => {
       const now = new Date();
       const diffInMs = Math.abs(now.getTime() - targetDate.getTime());
 
-      const years = Math.floor(diffInMs / (1000 * 60 * 60 * 24 * 365.25));
-      const months = Math.floor(diffInMs / (1000 * 60 * 60 * 24 * 30.44));
-      const days = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(diffInMs / (1000 * 60 * 60));
-      const minutes = Math.floor(diffInMs / (1000 * 60));
-      const seconds = Math.floor(diffInMs / 1000);
+      const years = {
+        label: "ano",
+        value: Math.floor(diffInMs / (1000 * 60 * 60 * 24 * 365.25)),
+      };
+      const months = {
+        label: "mes",
+        value: Math.floor(diffInMs / (1000 * 60 * 60 * 24 * 30.44)),
+      };
+      const days = {
+        label: "dia",
+        value: Math.floor(diffInMs / (1000 * 60 * 60 * 24)),
+      };
+      const hours = {
+        label: "hora",
+        value: Math.floor(diffInMs / (1000 * 60 * 60)),
+      };
+      const minutes = {
+        label: "minuto",
+        value: Math.floor(diffInMs / (1000 * 60)),
+      };
+      const seconds = {
+        label: "segundo",
+        value: Math.floor(diffInMs / 1000),
+      };
 
       setTimeDifference({ years, months, days, hours, minutes, seconds });
     };
@@ -33,15 +56,32 @@ const App: React.FC = () => {
     return () => clearInterval(interval);
   }, []);
 
+  const getLabel = (time: TimeDifference) => {
+    return time.value > 1 ? `${time.label}s` : time.label;
+  };
+
+  const getValue = (value: number) => {
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
   return (
     <div className="App">
-      <h1>Estamos juntos há:</h1>
-      <p className="clock">{timeDifference.years} anos</p>
-      <p> {timeDifference.months} meses</p>
-      <p> {timeDifference.days} dias</p>
-      <p> {timeDifference.hours} horas</p>
-      <p> {timeDifference.minutes} minutos</p>
-      <p> {timeDifference.seconds} segundos.</p>
+      <div>
+        <h1>Contagem regressiva para o casamento</h1>
+      </div>
+      <div>
+        <h1>Estamos juntos há:</h1> <br />
+        <div className="container">
+          {Object.entries(timeDifference).map(
+            (key, index) =>
+              key[1].value > 1 && (
+                <div className="timeCard" key={index}>
+                  <p>{`${getValue(key[1].value)} ${getLabel(key[1])}`}</p>
+                </div>
+              )
+          )}
+        </div>
+      </div>
     </div>
   );
 };
